64a3443dd934e6381a6d0fd3b36417142b187d09
IT21346286 <99608021+IT21346286@users.noreply.github.com>
2022-02-13 17:39:34 +0000

    I have completed exercise 1.

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 
 int main() {
+  int mark1, mark2, sum, average;
+  printf("Enter mark1:");
+  scanf("%d",&mark1);
+  printf("Enter mark2:");
+  scanf("%d",&mark2);
+  sum = mark1 + mark2;
+  average = sum/2;
+  printf("Average %d\n",average);
   
   return 0;
 }
